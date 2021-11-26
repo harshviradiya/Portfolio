85ff2a375161d465e343d8f6f5e35298cb7e6262
harshviradiya <harsh.viradiya@dignizant.com>
2021-11-26 18:52:31 +0530

    responsive

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -4,16 +4,16 @@ import './footer.scss'
 export default function Footer() {
   return (
     <div className="container" id="footer">
-    <div className="footer">
+      <div className="footer">
         <img src="./Assest/bgshape/logo.PNG" alt="" />
         <div className="menudiv">
-            <ul>
-              <li>© Untitled. All rights reserved.</li>
-              <li className="text">Design and Developed by Jemish Viradiya</li>
-              
-            </ul>
-            </div>
-    </div> 
-</div>
+          <ul>
+            <li>© Untitled. All rights reserved.</li>
+            <li className="text">Design and Developed by Jemish Viradiya</li>
+
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
